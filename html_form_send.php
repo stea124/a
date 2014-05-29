3dedb424c05b637cad224a899436bb8232abaeb7
Stephanie Tea <stephanie.tea@tiseagles.net>
2014-05-29 21:31:21 +0800

    Update html_form_send.php

--- html_form_send.php
+++ html_form_send.php
@@ -2,9 +2,9 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "you@yourdomain.com";
+    $email_to = "stephanie.tea@tiseagles.net";
      
-    $email_subject = "website html form submissions";
+    $email_subject = "Website Feedback";
      
      
     function died($error) {
